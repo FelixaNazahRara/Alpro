b26356dff9fb253358f3fa0b3e81e59a36796195
FelixaNazahRara <felixarara635@gmail.com>
2025-05-07 15:01:19 +0700

    Add files via upload
    Praktikum 1 Alpro

---
+++ 24221004_Felixa/src/pkg24221004_felixa/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg24221004_felixa;
+
+/**
+ *
+ * @author abdul
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int nim = 24221004;
+       String nama = "Felixa";
+       String alamat = "Bansari Kab Temanggung";
+       String TTL = "Temanggung, 14 Juni 2006";
+                       
+        System.out.println("biodata");
+        
+        System.out.println("NIM = "+nim);
+         System.out.println("NAMA = "+nama);
+         System.out.println("ALAMAT = "+alamat);
+         System.out.println("TTL = "+TTL);
+    }
+    
+}
